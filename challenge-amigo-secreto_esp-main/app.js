f522886c4c162ee767a005a63449b0aef7f33502
DannyGDR <gdrd305@gmail.com>
2025-03-18 00:09:37 -0400

    Add files via upload

---
+++ challenge-amigo-secreto_esp-main/app.js
@@ -0,0 +1,43 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigo = [];
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value;
+
+    if(!nombreAmigo){
+        alert("Debes Ingresar un Nombre");
+        return;
+    }
+
+    amigo.push(nombreAmigo);
+    inputAmigo.value = "";
+    inputAmigo.focus();
+    renderAmigos();
+}
+
+function renderAmigos() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    for (let i = 0; i < amigo.length; i++) {
+        let item = document.createElement("li");
+        item.textContent = amigo[i];
+        listaAmigos.appendChild(item);
+    }
+
+}
+
+function sortearAmigo(){
+    if(amigo.length === 0){
+        alert("No hay amigos para Sortear.");
+        return;
+    }
+    let amigoSorteado = amigo[ Math.floor(Math.random() * amigo.length) ];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `El amigo sorteado es ${amigoSorteado}`;
+
+    let clearList = document.getElementById("listaAmigos");
+    clearList.innerHTML = "";
+}
